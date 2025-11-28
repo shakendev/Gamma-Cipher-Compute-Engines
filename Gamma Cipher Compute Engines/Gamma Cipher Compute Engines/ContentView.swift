31b1f8cc3759396b05700ce8c5e29c03ab166d12
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-11-28 14:10:01 +0300

    feat: add an empty xcode proj

---
+++ Gamma Cipher Compute Engines/Gamma Cipher Compute Engines/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gamma Cipher Compute Engines
+//
+//  Created by Dimka Novikov on 28.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gamma Cipher Compute Engines/Gamma Cipher Compute Engines/Gamma_Cipher_Compute_EnginesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gamma_Cipher_Compute_EnginesApp.swift
+//  Gamma Cipher Compute Engines
+//
+//  Created by Dimka Novikov on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Gamma_Cipher_Compute_EnginesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
